39b35aca9790cb9c9cb2e205e193f45cac228ee2
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-05 23:11:40 +0300

    edit container

--- src/components/Load.jsx
+++ src/components/Load.jsx
@@ -23,24 +23,28 @@ function Load() {
         fetchCards()
     }, [])
     return (
-        <div className="container d-flex flex-wrap gap-1">
-            {errorMsg && <p>{ errorMsg }</p>}
-            {
-                cards && cards.length > 0 ? (
-                    cards.map((item) => (
-                        <div key={item.id} className="card" style={{width:"12rem"}}>
-                            <img src={item.images} className="card-img-top" alt={item.title} />
-                            <div className="card-body">
-                                <p className="card-text text-center">{ item.title }</p>
-                            </div>
-                        </div>
-                    ))
-                ) : (
-                    <div>No data fetched</div>
-                )
-            }
+      <div className="container">
+        <div className="d-flex flex-wrap gap-1 justify-content-center">
+          {errorMsg && <p>{errorMsg}</p>}
+          {cards && cards.length > 0 ? (
+            cards.slice(0, 6).map((item) => (
+              <div key={item.id} className="card" style={{ width: "12rem" }}>
+                <img
+                  src={item.images}
+                  className="card-img-top"
+                  alt={item.title}
+                />
+                <div className="card-body">
+                  <p className="card-text text-center">{item.title}</p>
+                </div>
+              </div>
+            ))
+          ) : (
+            <div>No data fetched</div>
+          )}
         </div>
-    )
+      </div>
+    );
 }
 
 export default Load
